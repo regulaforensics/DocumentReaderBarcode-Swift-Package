ddb8064154aaed940fcee508a7d62db1ae24fa65
Jenkins <jenkins@regula.by>
2026-06-30 17:50:57 +0300

    Release 9.6.18880

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["Barcode"]),
     ],
     targets: [
-        .binaryTarget(name: "Barcode", url: "https://pods.regulaforensics.com/Barcode/9.6.18854/DocumentReaderCore_barcode_9.6.18854.zip", checksum: "d4e446b049370a9ef797c91e0d6a78b1f656bdc04cc01223b075123c501abd02"),
+        .binaryTarget(name: "Barcode", url: "https://pods.regulaforensics.com/Barcode/9.6.18880/DocumentReaderCore_barcode_9.6.18880.zip", checksum: "dd6bb7e8d06e204182f56652c11c35f1bc51092f0b97c3eb29773a2ca6a0ee75"),
     ]
 )
